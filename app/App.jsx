879603158a003b8787f76819f0854094c6ce7deb
zhenzhen <wzzpdl@gmail.com>
2024-10-11 17:34:07 -0600

    lab2

---
+++ app/App.jsx
@@ -0,0 +1,22 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import ToDoForm from './ToDoForm';
+import ToDoList from './ToDoList';
+
+
+function App() {
+  return (
+    <View>
+      <ToDoList />
+      <ToDoForm />
+    </View>
+  );
+}
+
+export default App;
